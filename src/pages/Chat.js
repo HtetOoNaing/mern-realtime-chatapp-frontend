1e8786a861698cd9d475896d4ec8bf9650a30719
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2022-12-19 23:37:30 +0630

    update Chats page with components

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,21 +1,21 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { ChatState } from "../context/ChatProvider";
+import { Box } from "@chakra-ui/layout";
+import SideDrawer from "../components/miscellaneous/SideDrawer";
+import MyChats from "../components/MyChats";
+import ChatBox from "../components/ChatBox";
 
 const Chat = () => {
-  const [chats, setChats] = useState([]);
-  const fetchChats = async () => {
-    const { data } = await axios.get("/api/chat");
-    console.log("data", data);
-    setChats(data);
-  };
-  useEffect(() => {
-    fetchChats();
-  }, []);
+  const { user } = ChatState();
+  console.log("user", user);
   return (
-    <div>
-      {chats.map((chat) => (
-        <div key={chat._id}>{chat.chatName}</div>
-      ))}
+    <div style={{ width: "100%" }}>
+      {user && <SideDrawer />}
+      <Box>
+        {user && <MyChats />}
+        {user && <ChatBox />}
+      </Box>
     </div>
   );
 };
