1509a64de790a0bf0867b97edff9636fa7505a89
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2022-12-25 01:06:38 +0630

    add my chats page

--- src/components/MyChats.js
+++ src/components/MyChats.js
@@ -1,11 +1,11 @@
-import { useToast } from '@chakra-ui/react';
-import axios from 'axios';
-import React, { useState } from 'react'
-import { ChatState } from '../context/ChatProvider';
+import { Box, useToast } from "@chakra-ui/react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { ChatState } from "../context/ChatProvider";
 
 const MyChats = () => {
-  const [loggedUser, setLoggedUser] = useState()
-  const { user, setSelectedChat, chats, setChats  } = ChatState();
+  const [loggedUser, setLoggedUser] = useState();
+  const { user, selectedChat, setSelectedChat, chats, setChats } = ChatState();
 
   const toast = useToast();
 
@@ -17,7 +17,7 @@ const MyChats = () => {
         },
       };
       const { data } = await axios.get(`/api/chat`, config);
-      setChats(data)
+      setChats(data);
     } catch (error) {
       toast({
         title: "Error Occured!",
@@ -28,11 +28,27 @@ const MyChats = () => {
         position: "bottom-left",
       });
     }
-  }
+  };
+
+  console.log("chats", chats);
+
+  useEffect(() => {
+    setLoggedUser(JSON.parse(localStorage.getItem("userInfo")));
+    fetchChats();
+  }, []);
 
   return (
-    <div>MyChats</div>
-  )
-}
+    <Box
+      display={{ base: selectedChat ? "none" : "flex", md: "flex" }}
+      flexDir="column"
+      alignItems="center"
+      p="3"
+      bg="white"
+      w={{ base: "100%", md: "31%" }}
+      borderRadius="lg"
+      borderWidth="1px"
+    ></Box>
+  );
+};
 
-export default MyChats
+export default MyChats;
