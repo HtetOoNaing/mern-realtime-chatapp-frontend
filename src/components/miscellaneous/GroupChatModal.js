7592f4059e1c739881b6b154718aeb2c7c8f4673
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2023-01-07 23:06:26 +0630

    use multi select in GroupChatModal

--- src/components/miscellaneous/GroupChatModal.js
+++ src/components/miscellaneous/GroupChatModal.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   Modal,
   ModalOverlay,
@@ -12,41 +12,34 @@ import {
   useToast,
   FormControl,
   Input,
-  Box,
 } from "@chakra-ui/react";
+
 import { ChatState } from "../../context/ChatProvider";
 import axios from "axios";
-import UserListItem from "../userAvatar/UserListItem";
-import UserBadgeItem from "../userAvatar/UserBadgeItem";
 import { Select } from "chakra-react-select";
 
 const GroupChatModal = ({ children }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [groupChatName, setGroupChatName] = useState("");
-  const [selectedUsers, setSelectedUsers] = useState([]);
-  const [search, setSearch] = useState("");
-  const [searchResult, setSearchResult] = useState([]);
-  const [loading, setLoading] = useState(false);
   const toast = useToast();
-  const [value, setValue] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [selectedUsers, setSelectedUsers] = useState([]);
 
   const { user, chats, setChats } = ChatState();
 
-  const handleSearch = async (query) => {
-    setSearch(query);
-    if (!query) {
-      return;
-    }
+  const fetchUsers = async () => {
     try {
-      setLoading(true);
       const config = {
         headers: {
           Authorization: `Bearer ${user.token}`,
         },
       };
-      const { data } = await axios.get(`/api/user?search=${query}`, config);
-      setLoading(false);
-      setSearchResult(data);
+      const { data } = await axios.get(`/api/user`, config);
+      const userOptions = data.map((user) => ({
+        label: user.name,
+        value: user._id,
+      }));
+      setUsers(userOptions);
     } catch (error) {
       toast({
         title: "Error Occured!",
@@ -59,25 +52,9 @@ const GroupChatModal = ({ children }) => {
     }
   };
 
-  const handleGroup = (userToAdd) => {
-    if (selectedUsers.includes(userToAdd)) {
-      toast({
-        title: "User already added",
-        status: "warning",
-        duration: 5000,
-        isClosable: true,
-        position: "top",
-      });
-      return;
-    }
-    setSelectedUsers([...selectedUsers, userToAdd]);
-  };
-
-  const handleDelete = (userToRemove) => {
-    setSelectedUsers(
-      selectedUsers.filter((sel) => sel._id !== userToRemove._id)
-    );
-  };
+  useEffect(() => {
+    fetchUsers();
+  }, []);
 
   const handleSubmit = async () => {
     if (!groupChatName || selectedUsers.length <= 0) {
@@ -101,7 +78,7 @@ const GroupChatModal = ({ children }) => {
         `/api/chat/group`,
         {
           name: groupChatName,
-          users: JSON.stringify(selectedUsers.map((u) => u._id)),
+          users: JSON.stringify(selectedUsers.map((u) => u.value)),
         },
         config
       );
@@ -150,48 +127,14 @@ const GroupChatModal = ({ children }) => {
               />
             </FormControl>
             <FormControl>
-              <Input
-                placeholder="Add Users eg: John, Piyush, Jane"
-                mb={1}
-                onChange={(e) => handleSearch(e.target.value)}
-              />
-            </FormControl>
-            <Box w="100%" display="flex" flexWrap="wrap">
-              {selectedUsers.map((u) => (
-                <UserBadgeItem
-                  key={u._id}
-                  user={u}
-                  handleFunction={() => handleDelete(u)}
-                />
-              ))}
-            </Box>
-            {loading ? (
-              <div>loading...</div>
-            ) : (
-              searchResult
-                ?.slice(0, 4)
-                .map((user) => (
-                  <UserListItem
-                    key={user._id}
-                    user={user}
-                    handleClick={() => handleGroup(user)}
-                  />
-                ))
-            )}
-            <FormControl>
               <Select
                 isMulti
                 name="colors"
-                options={[
-                  { value: "blue", label: "Blue", color: "#0052CC" },
-                  { value: "purple", label: "Purple", color: "#5243AA" },
-                  { value: "red", label: "Red", color: "#FF5630" },
-                  { value: "orange", label: "Orange", color: "#FF8B00" },
-                  { value: "yellow", label: "Yellow", color: "#FFC400" },
-                  { value: "green", label: "Green", color: "#36B37E" },
-                ]}
+                options={users}
                 placeholder="Select some colors..."
                 closeMenuOnSelect={false}
+                value={selectedUsers}
+                onChange={setSelectedUsers}
               />
             </FormControl>
           </ModalBody>
